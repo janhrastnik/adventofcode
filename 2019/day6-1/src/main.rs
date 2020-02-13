d7a00f725039275cfb77bbf300aea66c97d470a3
Jan Hrastnik <jan.hrastnik2@gmail.com>
2020-02-13 23:58:32 +0100

    2019 6-1

--- 2019/day6-1/src/main.rs
+++ 2019/day6-1/src/main.rs
@@ -7,43 +7,50 @@ fn get_node_distance(
     mut counter: usize,
     nodesmap: &mut HashMap<&str, Vec<&str>>,
 ) -> usize {
-    println!("{}", curr_node);
     if curr_node == target_node {
         return counter;
     } else {
         if let Some(nodesvec) = nodesmap.get_mut(curr_node) {
+            let newnodesvec = nodesvec.clone();
             counter += 1;
-            println!("{:?}", nodesvec);
-            if nodesvec.contains(&target_node) {
-                println!("found it");
-                return counter;
-            } else {
-                for node in nodesvec {
-                    return get_node_distance(node, target_node, counter, nodesmap);
+            for node in newnodesvec {
+                let distance = get_node_distance(node, target_node, counter, nodesmap);
+                if distance == 0 {
+                    continue;
+                } else {
+                    return distance;
                 }
-                counter
             }
+            0
         } else {
-            println!("{}", curr_node);
-            counter
+            0
         }
     }
 }
 
 fn main() {
-    let data = fs::read_to_string("input").expect("Error");
+    let data = fs::read_to_string("realinput").expect("Error");
     let vec: Vec<&str> = data.split_whitespace().collect();
-    let mut nodes: HashMap<&str, Vec<_>> = HashMap::new();
+    let mut graph: HashMap<&str, Vec<_>> = HashMap::new();
+    let mut keys: Vec<&str> = vec![];
     for instruction in vec {
         let orbit: Vec<&str> = instruction.split(")").collect();
-        if !nodes.contains_key(orbit[0]) {
+        if !keys.contains(&orbit[0]) {
+            keys.push(orbit[0]);
+        }
+        if !keys.contains(&orbit[1]) {
+            keys.push(orbit[1]);
+        }
+        if !graph.contains_key(orbit[0]) {
             let neighbours = vec![orbit[1]];
-            nodes.insert(orbit[0], neighbours);
+            graph.insert(orbit[0], neighbours);
         } else {
-            nodes.get_mut(orbit[0]).unwrap().push(orbit[1]);
+            graph.get_mut(orbit[0]).unwrap().push(orbit[1]);
         }
     }
-    println!("{:?}", nodes);
-    let distance = get_node_distance("COM", "H", 0, &mut nodes);
-    println!("{}", distance);
+    let mut counter = 0;
+    for node in keys {
+        counter += get_node_distance("COM", node, 0, &mut graph);
+    }
+    println!("{}", counter);
 }
