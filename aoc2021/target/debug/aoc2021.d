348afe14dff0cd06acb7b39030b24b90fe4addc3
Jan Hrastnik <jan.hrastnik2@gmail.com>
2021-12-01 21:47:41 +0100

    day one refactoring

--- aoc2021/target/debug/aoc2021.d
+++ aoc2021/target/debug/aoc2021.d
@@ -1,1 +1,1 @@
-/home/jan/rust/adventofcode/aoc2021/target/debug/aoc2021: /home/jan/rust/adventofcode/aoc2021/src/day1.rs /home/jan/rust/adventofcode/aoc2021/src/main.rs
+C:\Users\janhr\Documents\adventofcode\aoc2021\target\debug\aoc2021.exe: C:\Users\janhr\Documents\adventofcode\aoc2021\src\day1.rs C:\Users\janhr\Documents\adventofcode\aoc2021\src\main.rs
