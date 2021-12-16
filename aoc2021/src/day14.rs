6391af61803cb4e38df24667ff01213e50a4cd51
Jan Hrastnik <jan.hrastnik2@gmail.com>
2021-12-16 14:54:57 +0100

    2021 day fourteen

--- aoc2021/src/day14.rs
+++ aoc2021/src/day14.rs
@@ -3,13 +3,6 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn into_character_map(word: &str) -> HashMap<char, i32> {
-    word.chars().fold(HashMap::new(), |mut acc, c| {
-        *acc.entry(c).or_insert(0) += 1;
-        acc
-    })
-}
-
 fn most_frequent_char<K, V>(a_hash_map: &HashMap<K, V>) -> Option<&K>
 where
     V: Ord,
@@ -36,38 +29,55 @@ fn solve(steps: usize) -> usize {
         .lines()
         .map(|x| x.unwrap())
         .collect();
-    let mut polymer_template: String = lines[0].to_string();
+    let polymer_template: Vec<char> = lines[0].chars().collect();
     let mut rules: HashMap<String, char> = HashMap::new();
     for line in lines.iter().skip(2) {
         let rule: Vec<&str> = line.split(" -> ").collect();
         rules.insert(rule[0].to_string(), rule[1].chars().next().unwrap());
     }
-    println!("{:?}", rules);
-
-    for step in 0..steps {
-        println!("{}", step);
-        let mut matched_pairs: HashMap<usize, char> = HashMap::new();
-        for (i, char_tuple) in polymer_template
-            .chars()
-            .zip(polymer_template.chars().skip(1))
-            .enumerate()
-        {
-            let mut polymer_slice = String::from("");
-            polymer_slice.push(char_tuple.0);
-            polymer_slice.push(char_tuple.1);
-            if rules.contains_key(&polymer_slice) {
-                matched_pairs.insert(i + 1 + matched_pairs.len(), rules[&polymer_slice[..]]);
-            }
-        }
-        for key in matched_pairs.keys().sorted() {
-            polymer_template.insert(*key, matched_pairs[key]);
+    // get the starting pairs, store them in a hashmap
+    let mut starting_pairs: HashMap<String, usize> = HashMap::new();
+    for i in 0..polymer_template.len() {
+        let b = polymer_template.get(i + 1);
+        if b.is_some() {
+            let a = polymer_template.get(i);
+            let mut pair = String::new();
+            pair.push(*a.unwrap());
+            pair.push(*b.unwrap());
+            *starting_pairs.entry(pair).or_insert(0) += 1;
         }
     }
-    let char_map = into_character_map(&polymer_template[..]);
-    let max = char_map[most_frequent_char(&char_map).unwrap()];
-    let min = char_map[least_frequent_char(&char_map).unwrap()];
-    println!("{}, {}, {}", polymer_template.len(), max, min);
-    // println!("{:?}", matched_pairs);
+
+    // get the count of characters for starting template
+    let mut character_count_map = polymer_template.into_iter().counts();
+
+    for _step in 0..steps {
+        // iterate over the pairs, match them with rules
+        let mut new_pairs = starting_pairs.clone();
+        for pair in starting_pairs.keys() {
+            if rules.contains_key(pair) {
+                // on match, remove the matched pair, add the two new pairs
+                // increase character count by the matched character
+                let count = starting_pairs.get(pair).unwrap();
+                let x = *rules.get(pair).unwrap();
+                *new_pairs.entry(pair.clone()).or_insert(0) -= count;
+                *character_count_map.entry(x).or_insert(0) += count;
+                let a = pair.chars().next().unwrap();
+                let b = pair.chars().last().unwrap();
+                let mut ax: String = String::new();
+                ax.push(a);
+                ax.push(x);
+                let mut xb: String = String::new();
+                xb.push(x);
+                xb.push(b);
+                *new_pairs.entry(ax).or_insert(0) += count;
+                *new_pairs.entry(xb).or_insert(0) += count;
+            }
+        }
+        starting_pairs = new_pairs.clone();
+    }
+    let max = character_count_map[most_frequent_char(&character_count_map).unwrap()];
+    let min = character_count_map[least_frequent_char(&character_count_map).unwrap()];
 
     (max - min) as usize
 }
