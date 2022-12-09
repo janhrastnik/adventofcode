bf7adc421685bf1f304fffc00ec5cc3d4d06c0d8
Jan Hrastnik <jan.hrastnik2@gmail.com>
2022-12-09 19:38:18 +0100

    2022 day 9 part 1 refactor

--- aoc2022/src/day9.rs
+++ aoc2022/src/day9.rs
@@ -36,12 +36,8 @@ fn part_one() {
                             head.x += 1;
 
                             // move tail
-                            if (head.x - tail.x).abs() > 1 {
-                                if head.y != tail.y {
-                                    tail.y = head.y;
-                                }
-                                tail.x += 1;
-                            }
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
                             visited.insert(tail.clone(), true);
                         }
                     }
@@ -52,12 +48,8 @@ fn part_one() {
                             head.x -= 1;
 
                             // move tail
-                            if (head.x - tail.x).abs() > 1 {
-                                if head.y != tail.y {
-                                    tail.y = head.y;
-                                }
-                                tail.x -= 1;
-                            }
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
                             visited.insert(tail.clone(), true);
                         }
                     }
@@ -67,13 +59,8 @@ fn part_one() {
                             // move head
                             head.y += 1;
 
-                            // move tail
-                            if (head.y - tail.y).abs() > 1 {
-                                if head.x != tail.x {
-                                    tail.x = head.x;
-                                }
-                                tail.y += 1;
-                            }
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
                             visited.insert(tail.clone(), true);
                         }
                     }
@@ -84,12 +71,8 @@ fn part_one() {
                             head.y -= 1;
 
                             // move tail
-                            if (head.y - tail.y).abs() > 1 {
-                                if head.x != tail.x {
-                                    tail.x = head.x;
-                                }
-                                tail.y -= 1;
-                            }
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
                             visited.insert(tail.clone(), true);
                         }
                     }
@@ -98,90 +81,54 @@ fn part_one() {
             };
             //println!("{:?}", visited.keys().len());
         }
-        println!("PART ONE: {:?}", visited.keys().len());
+        println!("PART TWO: {:?}", visited.keys().len());
     }
 }
 
 fn part_two() {
-    if let Ok(lines) = files::shared::read_lines("./input/day9test2.txt") {
+    if let Ok(lines) = files::shared::read_lines("./input/day9.txt") {
         let mut visited: HashMap<Point, bool> = HashMap::new();
         let mut head = Point { x: 0, y: 0 };
         let mut tail = Point { x: 0, y: 0 };
-        let mut rope: Vec<Point> = vec![];
-        for _i in 0..10 {
-            rope.push(Point { x: 0, y: 0 });
-        }
-        visited.insert(rope.last().unwrap().clone(), true);
+        visited.insert(tail.clone(), true);
 
         for line in lines {
             if let Ok(command) = line {
-                println!("{:?}", command);
                 let command_vec: Vec<&str> = command.split_whitespace().collect::<Vec<&str>>();
                 match command_vec[0] {
                     "R" => {
                         let count = command_vec[1].parse::<isize>().unwrap();
                         for _i in 0..count {
                             // move head
-                            rope[0].x += 1;
+                            head.x += 1;
 
-                            for knot in 0..(rope.len() - 1) {
-                                // move tail
-                                if ((rope[knot].x - rope[knot + 1].x).abs() == 2)
-                                    && ((rope[knot].y - rope[knot + 1].y).abs() == 2)
-                                {
-                                    rope[knot + 1].x += 1;
-                                    rope[knot + 1].y += (rope[knot].y - rope[knot + 1].y) / 2;
-                                } else if (rope[knot].x - rope[knot + 1].x).abs() == 2 {
-                                    rope[knot + 1].x += 1;
-                                }
-                            }
-
-                            visited.insert(rope.last().unwrap().clone(), true);
+                            // move tail
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
+                            visited.insert(tail.clone(), true);
                         }
                     }
                     "L" => {
                         let count = command_vec[1].parse::<isize>().unwrap();
                         for _i in 0..count {
                             // move head
-                            rope[0].x -= 1;
+                            head.x -= 1;
 
-                            for knot in 0..(rope.len() - 1) {
-                                // move tail
-                                if ((rope[knot].x - rope[knot + 1].x).abs() == 2)
-                                    && ((rope[knot].y - rope[knot + 1].y).abs() == 2)
-                                {
-                                    rope[knot + 1].x -= 1;
-                                    rope[knot + 1].y -= (rope[knot].y - rope[knot + 1].y) / 2;
-                                } else if (rope[knot].x - rope[knot + 1].x).abs() == 2 {
-                                    rope[knot + 1].x -= 1;
-                                }
-                            }
-
-                            visited.insert(rope.last().unwrap().clone(), true);
+                            // move tail
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
+                            visited.insert(tail.clone(), true);
                         }
                     }
                     "U" => {
                         let count = command_vec[1].parse::<isize>().unwrap();
                         for _i in 0..count {
                             // move head
-                            rope[0].y += 1;
+                            head.y += 1;
 
-                            for knot in 0..(rope.len() - 1) {
-                                // move tail
-
-                                if ((rope[knot].y - rope[knot + 1].y).abs() == 2)
-                                    && (rope[knot].x - rope[knot + 1].x).abs() == 1
-                                {
-                                    rope[knot + 1].y += 1;
-                                    rope[knot + 1].x += 1;
-                                } else if (rope[knot].x - rope[knot + 1].x).abs() == 2 {
-                                    rope[knot + 1].x += 1;
-                                } else if (rope[knot].y - rope[knot + 1].y).abs() == 2 {
-                                    rope[knot + 1].y += 1;
-                                }
-                            }
-
-                            visited.insert(rope.last().unwrap().clone(), true);
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
+                            visited.insert(tail.clone(), true);
                         }
                     }
                     "D" => {
@@ -189,28 +136,32 @@ fn part_two() {
                         for _i in 0..count {
                             // move head
                             head.y -= 1;
-                            rope[0].y -= 1;
 
-                            for knot in 0..(rope.len() - 1) {
-                                // move tail
-                                if ((rope[knot].x - rope[knot + 1].x).abs() == 2)
-                                    && ((rope[knot].y - rope[knot + 1].y).abs() == 2)
-                                {
-                                    rope[knot + 1].y -= 1;
-                                    rope[knot + 1].x -= (rope[knot].x - rope[knot + 1].x) / 2;
-                                } else if (rope[knot].y - rope[knot + 1].y).abs() == 2 {
-                                    rope[knot + 1].y -= 1;
-                                }
-                            }
-                            visited.insert(rope.last().unwrap().clone(), true);
+                            // move tail
+                            tail_check(&mut head, &mut tail);
+                            println!("{:?}", tail);
+                            visited.insert(tail.clone(), true);
                         }
                     }
                     _ => unreachable!(),
                 }
             };
-            println!("{:?}", rope);
-            println!("{:?}", visited.keys().len());
+            //println!("{:?}", visited.keys().len());
         }
-        println!("PART ONE:")
+        println!("PART TWO: {:?}", visited.keys().len());
+    }
+}
+
+fn tail_check(head: &mut Point, tail: &mut Point) {
+    if ((head.x - tail.x).abs() == 2) && ((head.y - tail.y).abs() == 1) {
+        tail.x += (head.x - tail.x) / 2;
+        tail.y += head.y - tail.y;
+    } else if ((head.x - tail.x).abs() == 1) && ((head.y - tail.y).abs() == 2) {
+        tail.x += head.x - tail.x;
+        tail.y += (head.y - tail.y) / 2;
+    } else if (head.x - tail.x).abs() == 2 {
+        tail.x += (head.x - tail.x) / 2;
+    } else if (head.y - tail.y).abs() == 2 {
+        tail.y += (head.y - tail.y) / 2;
     }
 }
